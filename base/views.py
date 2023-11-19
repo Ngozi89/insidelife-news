9d75672351db54b01855d76444014215d118331e
Ngozi89 <>
2023-11-19 22:19:37 +0000

    Create post view

--- base/views.py
+++ base/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 # Create your views here.
+
+
+class PostList(generic.ListView):
+    model = Post
+# Render list of post    
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+# Seperate the pages    
+    paginate_by = 6
